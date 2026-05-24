2c32099b9dee2f2499adb73fe7a1a0cf06abfd32
mailbotprem-haha <mailbotprem@gmail.com>
2026-05-24 14:03:05 +0530

    Update index.mjs

--- backend_task3/index.mjs
+++ backend_task3/index.mjs
@@ -13,7 +13,9 @@ const app = express();
 const PORT =
   process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://jira-clone-xi.vercel.app",
+}));
 
 app.use(express.json());
 
